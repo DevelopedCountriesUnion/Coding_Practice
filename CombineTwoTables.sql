e4f3be27ffe6d2b10cb170e1f11b715c7a2719a9
DevelopedCountriesUnion <50728615+DevelopedCountriesUnion@users.noreply.github.com>
2021-09-01 17:26:42 +1200

    Update CombineTwoTables.sql

--- CombineTwoTables.sql
+++ CombineTwoTables.sql
@@ -28,8 +28,9 @@ Write a SQL query for a report that provides the following information for each 
 FirstName, LastName, City, State
 
 =============================================================================================================
+Since the PersonId in table Address is the foreign key of table Person, we can join this two table to get the address information of a person.
+Considering there might not be an address information for every person, we should use outer join instead of the default inner join.
 
-// Solution codes
 */
 select FirstName, LastName, City, State
 from Person 
